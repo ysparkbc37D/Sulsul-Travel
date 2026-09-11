7405a5cf93860f2d4e29abd08c875bd17b6ad4ac
yspark <ysparkbc37@gmail.com>
2026-09-11 16:48:34 +0900

    feat: 여행 가계부 1급 탭 복원 및 다차원 자연 지출 업데이트 구축 (v1.4.7)

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 // Sulsul-Travel Service Worker
-const V = 'st-shell-v1.4.6';
+const V = 'st-shell-v1.4.7';
 const CACHE_NAME = V;
 const CACHE_PREFIX = 'st-shell-';
 
 const PRECACHE_ASSETS = [
   './',
   './index.html',
-  './css/utilities.css?v=1.4.6',
-  './css/companion.css?v=1.4.6',
-  './js/presentation/companion-ui.js?v=1.4.6',
+  './css/utilities.css?v=1.4.7',
+  './css/companion.css?v=1.4.7',
+  './js/presentation/companion-ui.js?v=1.4.7',
   './vendor/fontawesome/css/all.min.css',
   './vendor/fontawesome/webfonts/fa-solid-900.woff2',
   './vendor/fontawesome/webfonts/fa-regular-400.woff2',
