30e2f36a9156025efcf23a9e96f0d0c3a8d2c635
yspark <ysparkbc37@gmail.com>
2026-09-13 07:29:31 +0900

    feat(branding): update app icons and mascot with book-reading bear design (v1.6.5)

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 // Sulsul-Travel Service Worker
-const V = 'st-shell-v1.6.4';
+const V = 'st-shell-v1.6.5';
 const CACHE_NAME = V;
 const CACHE_PREFIX = 'st-shell-';
 
 const PRECACHE_ASSETS = [
   './',
   './index.html',
-  './css/utilities.css?v=1.6.4',
-  './css/companion.css?v=1.6.4',
-  './js/presentation/companion-ui.js?v=1.6.4',
+  './css/utilities.css?v=1.6.5',
+  './css/companion.css?v=1.6.5',
+  './js/presentation/companion-ui.js?v=1.6.5',
   './vendor/fontawesome/css/all.min.css',
   './vendor/fontawesome/webfonts/fa-solid-900.woff2',
   './vendor/fontawesome/webfonts/fa-regular-400.woff2',
