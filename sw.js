f9e9aa1d4376e27e8dc1bba506ffe2bcaba51234
yspark <ysparkbc37@gmail.com>
2026-09-13 06:29:00 +0900

    feat(branding): add vintage bear mascot, cursive script logo, single-line slogan and new app icons (v1.6.4)

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 // Sulsul-Travel Service Worker
-const V = 'st-shell-v1.6.3';
+const V = 'st-shell-v1.6.4';
 const CACHE_NAME = V;
 const CACHE_PREFIX = 'st-shell-';
 
 const PRECACHE_ASSETS = [
   './',
   './index.html',
-  './css/utilities.css?v=1.6.3',
-  './css/companion.css?v=1.6.3',
-  './js/presentation/companion-ui.js?v=1.6.3',
+  './css/utilities.css?v=1.6.4',
+  './css/companion.css?v=1.6.4',
+  './js/presentation/companion-ui.js?v=1.6.4',
   './vendor/fontawesome/css/all.min.css',
   './vendor/fontawesome/webfonts/fa-solid-900.woff2',
   './vendor/fontawesome/webfonts/fa-regular-400.woff2',
@@ -32,6 +32,7 @@ const PRECACHE_ASSETS = [
   './js/application/orchestration.mjs',
   './js/domain/replan.mjs',
   './south_america_illustrated_map.jpg',
+  './icons/sulsul_bear_mascot.png',
   './icons/icon-192.png',
   './icons/icon-512.png',
   './icons/icon-maskable-192.png',
